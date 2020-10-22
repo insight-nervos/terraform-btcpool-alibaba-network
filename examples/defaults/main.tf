5fc7084b9b79c1b192ceb3fe7031fd517e4543bd
Richard Mah <dick.mah@gmail.com>
2020-10-22 14:50:19 -0400

    update example update default test

--- examples/defaults/main.tf
+++ examples/defaults/main.tf
@@ -1,14 +1,9 @@
-variable "alicloud_region" {
-  default = "us-east-1"
-}
-
 provider "alicloud" {
   region = var.alicloud_region
 }
 
 module "defaults" {
   source   = "../.."
-  vpc_name = "example"
+  vpc_name = var.vpc_name
   num_azs  = 2
-
 }
---
+++ examples/defaults/variables.tf
@@ -0,0 +1,7 @@
+variable "alicloud_region" {
+  default = "us-east-1"
+}
+
+variable "vpc_name" {
+  default = "example"
+}
